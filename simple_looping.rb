db0f1ed4d5a334a9bc17e70b2da0f0c0e4fa32df
Rene Cocom <fasnimo@gmail.com>
2019-03-26 01:47:15 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,10 @@
 
 def loop_iterator(number_of_times)
  loop do
-   
+   number_of_times -= 1
     puts "Welcome to Flatiron School's Web Development Course!"
+    if number_of_times == 0
+      break
 end
 end
 
