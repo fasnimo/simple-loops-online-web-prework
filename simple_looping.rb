54a13a658940f108740e13c87ca94b90b013b411
Rene Cocom <fasnimo@gmail.com>
2019-03-26 01:48:12 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,33 +9,33 @@ end
 end
 end
 
-def times_iterator(number_of_times)
-   7. times do
-    puts "Welcome to Flatiron School's Web Development Course!"
-end
+# def times_iterator(number_of_times)
+#   7. times do
+#     puts "Welcome to Flatiron School's Web Development Course!"
+# end
   
-end
+# end
 
-def while_iterator(number_of_times)
-  number_of_times = 7
-  while number_of_times < 10
-  puts "Welcome to Flatiron School's Web Development Course!"
-  number_of_times += 1
-end
-end
+# def while_iterator(number_of_times)
+#   number_of_times = 7
+#   while number_of_times < 10
+#   puts "Welcome to Flatiron School's Web Development Course!"
+#   number_of_times += 1
+# end
+# end
 
-def until_iterator(number_of_times)
-  number_of_times = 7
- until number_of_times < 10
-  puts "Welcome to Flatiron School's Web Development Course!"
-  number_of_times += 1
-end
-end
+# def until_iterator(number_of_times)
+#   number_of_times = 7
+# until number_of_times < 10
+#   puts "Welcome to Flatiron School's Web Development Course!"
+#   number_of_times += 1
+# end
+# end
 
-def for_iterator(number_of_times)
-  number_of_times = 1..10
-  for trail_test in number_of_times
-  puts "Welcome to Flatiron School's Web Development Course!"
-end
-end
+# def for_iterator(number_of_times)
+#   number_of_times = 1..10
+#   for trail_test in number_of_times
+#   puts "Welcome to Flatiron School's Web Development Course!"
+# end
+# end
 
